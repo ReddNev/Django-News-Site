bf5709c229bdc237deb52a18be0b36f2abc12f72
ReddNev <creedlll@mail.ru>
2022-09-05 11:18:39 +0300

    update

--- my_site/news/models.py
+++ my_site/news/models.py
@@ -9,7 +9,7 @@ class News(models.Model):
     update_at = models.DateTimeField(auto_now=True, verbose_name='Обнавлено')
     photo = models.ImageField(blank=True, upload_to='photos/%Y/%m/%d/', verbose_name='Фото')
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='категория')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='категория')
 
     def get_absolute_url(self):
         return reverse('view_news', kwargs={'news_id': self.pk})
