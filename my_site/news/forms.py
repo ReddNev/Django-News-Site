b420509f6eb5fbef26c30a703987e94f10e885e0
ReddNev <creedlll@mail.ru>
2022-10-30 09:45:45 +0300

    update registration

--- my_site/news/forms.py
+++ my_site/news/forms.py
@@ -1,8 +1,30 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 import re
 
 from .models import News
+
+
+class UserRegisterForm(UserCreationForm):
+
+    username = forms.CharField(label='Имя пользователя',
+                               widget=forms.TextInput(attrs={'class': 'form-control', 'autocomplete': 'off'}))
+    first_name = forms.CharField(label='Имя',
+                                 widget=forms.TextInput(attrs={'class': 'form-control', 'autocomplete': 'off'}))
+    last_name = forms.CharField(label='Фамилия',
+                                widget=forms.TextInput(attrs={'class': 'form-control', 'autocomplete': 'off'}))
+    password1 = forms.CharField(label='Пароль',
+                                widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label='Подтверждение пароля',
+                                widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(label='Email',
+                             widget=forms.EmailInput(attrs={'class': 'form-control', 'autocomplete': 'off'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
 
 
 class NewsForm(forms.ModelForm):
