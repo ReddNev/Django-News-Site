4721d858d1190e1ede54532dffa7397e25cfb640
ReddNev <creedlll@mail.ru>
2022-11-01 15:46:43 +0300

    [ADD] sending emails

--- my_site/news/forms.py
+++ my_site/news/forms.py
@@ -7,6 +7,13 @@ import re
 from .models import News
 
 
+class ContactForm(forms.Form):
+    subject = forms.CharField(label='Тема',
+                              widget=forms.TextInput(attrs={'class': 'form-control', 'autocomplete': 'off'}))
+    content = forms.CharField(label='Текст',
+                              widget=forms.Textarea(attrs={'class': 'form-control', 'autocomplete': 'off', 'rows': 5}))
+
+
 class UserLoginForm(AuthenticationForm):
     username = forms.CharField(label='Имя пользователя',
                                widget=forms.TextInput(attrs={'class': 'form-control'}))
